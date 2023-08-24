8014ecc651719321e5e7c051d1a1134cf9f72e9b
Arjay Bayona <arjayby1@gmail.com>
2023-08-24 20:26:21 +0800

    Add group utility

--- src/components/button-link/ButtonLink.tsx
+++ src/components/button-link/ButtonLink.tsx
@@ -5,11 +5,10 @@ export interface ButtonLinkProps
 
 export const ButtonLink = (props: ButtonLinkProps) => {
   return (
-    <a
-      className="font-bold underline underline-offset-[12px] decoration-primary"
-      {...props}
-    >
-      {props.children}
+    <a href="#" className="font-bold group">
+      <span className="pb-2 bg-left-bottom bg-gradient-to-r from-primary to-primary bg-[length:0%_1px] bg-no-repeat group-hover:bg-[length:100%_1px] transition-all duration-500 ease-out">
+        {props.children}
+      </span>
     </a>
   );
 };
--- src/components/image/Image.tsx
+++ src/components/image/Image.tsx
@@ -6,12 +6,11 @@ export interface ImageProps extends ImageType {
 
 export const Image = ({ src, alt, withBorderBottom }: ImageProps) => {
   return (
-    <img
-      className={`w-[400px] ${
-        withBorderBottom ? "border-b-4 border-primary" : ""
-      }`}
-      src={src}
-      alt={alt}
-    />
+    <div className="relative group">
+      {withBorderBottom && (
+        <div className="absolute bottom-0 h-1 w-full bg-left bg-gradient-to-r from-primary to-primary bg-[length:0%_4px] bg-no-repeat group-hover:bg-[length:100%_4px] transition-all duration-500 ease-out" />
+      )}
+      <img className="w-[400px]" src={src} alt={alt} />
+    </div>
   );
 };
